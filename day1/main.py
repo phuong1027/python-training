4693adda2543d7249cf1ec0f4ff69f4fb92c0c9b
phuong1027 <phuong.phan1027@gmail.com>
2023-03-01 18:55:28 +0100

    day 1 - new

---
+++ day1/main.py
@@ -0,0 +1,24 @@
+"""
+multicomment
+...
+...
+"""
+# a single comment
+# value: -1, 3, 3.5, "Phuong", True, False
+print(type(4)) # int
+print(type(3.5)) # float
+print(type(True)) # bool
+print(type("Hello")) # str
+
+# variable - a name for a value
+age = 100
+print(age)
+
+age = 200
+print(age)
+
+# 1a = 5 - Ctrl + / - comment shortcut
+user_name = "phuong1027"
+print(user_name)
+
+print(1, 2, 3, 4)
