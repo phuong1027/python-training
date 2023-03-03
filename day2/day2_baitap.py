85e91ab0a00a0975d1021f7a2ad915fd249751e1
phuong1027 <phuong.phan1027@gmail.com>
2023-03-03 15:38:21 +0100

    day 3 - new

---
+++ day2/day2_baitap.py
@@ -0,0 +1,33 @@
+#Bai 1: Cho biết kiểu của các giá trị sau: -50, 1.456, False, 0.0, .5, 4+5j
+print(type(-50))
+print(type(1.456))
+print(type(False))
+print(type(0.0))
+print(type(.5))
+print(type(4+5j))
+
+#Bai 2: Định nghĩa hai biến num1, num2 có cùng giá trị
+number1 = 5
+number2 = 5
+print(number1, number2)
+
+#Bai 3: Định nghĩa hai biến number1, number2 tích của chúng bằng 49
+number1 = 7
+number2 = 7
+print(number1*number2)
+
+#Bai 4: Nhập vào tên và tuổi của bạn và in ra tên và tuổi của bạn trên cùng một hàng cách nhau bởi dấu | (xổ)
+Name = input('Enter your Name: ')
+Age = input('enter your age: ')
+print(Name | Age)
+#???
+
+#Bai 5: Nhập vào một số nguyên n, số thực z, chuỗi s và in các giá trị của chúng trên các dòng riêng biệt
+n = int(input('Enter a integer number: '))
+z = float(input('Enter a float number:'))
+s = str(input('Enter a string'))
+
+print(n)
+print(z)
+print(s)
+#???
